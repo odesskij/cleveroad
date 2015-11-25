744abb7025a37ecd26547a370bdc6f77a72c5aa2
Vladimir Odesskij <odesskij1992@gmail.com>
2015-11-25 13:55:50 +0200

    log4js tst

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var express = require('express')
     , path = require('path')
     , logger = require('morgan')
+    , log4js = require('log4js')
     , cookieParser = require('cookie-parser')
     , HTTPStatus = require('http-status')
     , bodyParser = require('body-parser')
@@ -9,15 +12,36 @@ var express = require('express')
     , apiUser = require('./src/controller/api.user')
     , apiItem = require('./src/controller/api.item');
 
+log4js.configure({
+    appenders: [
+        {type: 'console'},
+        {type: 'file', filename: 'app/logs/cheese.log', category: 'cheese', "backups": 3}
+    ]
+});
+
+
+var log = log4js.getLogger('cheese');
+
+log.setLevel('TRACE');
+log.trace('Entering cheese testing');
+log.debug('Got cheese.');
+log.info('Cheese is Gouda.');
+log.warn('Cheese is quite smelly.');
+log.error('Cheese is too ripe!');
+log.fatal('Cheese was breeding ground for listeria.');
+
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-if(process.env.NODE_ENV === 'dev'){
-    app.use(logger('dev'));
+if(process.env.NODE_ENV === 'dev') {
+    app.use(log4js.connectLogger(log, {level: 'TRACE'}));
 }
+
+app.use(log4js.connectLogger(log, {level: 'TRACE'}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
